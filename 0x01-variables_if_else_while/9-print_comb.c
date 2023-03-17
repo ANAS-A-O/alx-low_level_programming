8ed8e111990b6e62eebbfacb93dbb53968e96f79
root <root@LAPTOP-5K5T48B5>
2023-03-17 19:18:12 +0100

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 0; n < 9; n++)
 	{
 		putchar(n + '0');
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('\n');
+	putchar('9');
 	return (0);
 }
