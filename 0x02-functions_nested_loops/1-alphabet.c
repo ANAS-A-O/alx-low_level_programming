789a10c5548c15287101d644c9567eed54833b38
root <root@LAPTOP-5K5T48B5>
2023-03-21 22:09:51 +0000

    alphabetic

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet - print all alphabet in lowercase
+*/
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
+
