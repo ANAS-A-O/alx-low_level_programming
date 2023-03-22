868bc7926f33bc570dfd14edbee0487361a8652b
root <root@LAPTOP-5K5T48B5>
2023-03-22 21:12:35 +0000

    0 is posi

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* positive_or_negative - see if a given number positive or negative
+*
+* @i: the number to check
+*/
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
