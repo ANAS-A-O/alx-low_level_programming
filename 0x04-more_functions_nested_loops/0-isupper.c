c7ab447582d63f1a30b4664b592a0475dd983f3e
root <root@LAPTOP-5K5T48B5>
2023-03-23 15:39:18 +0000

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isupper(int c)- chek if c is upper
+* @c: int for check
+* Return: 0 if c is lower and 1 if c is upper.
+*/
+int _isupper(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
