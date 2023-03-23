30bc53d6ab57b3e8b9f425d87b941dfdfeff7489
root <root@LAPTOP-5K5T48B5>
2023-03-23 18:29:40 +0000

    is upper

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - prints the numbers from 1 to 100, followed by a new line
  * but for multiples of three prints Fizz instead of the number
@@ -14,17 +14,19 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
-		} else if (i % 5 == 0 )
+			printf(" FizzBuzz");
+		} else if (i % 5 == 0)
 		{
-			printf("Buzz ");
-		} else if (i % 3 == 0 )
+			printf(" Buzz");
+		} else if (i % 3 == 0)
 		{
-			printf("Fizz ");
-		
+			printf(" Fizz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
 		} else
 		{
-			printf("%d ", i);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
