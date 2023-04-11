8e5fd7930f0092ff8435b62989126115eec0eb82
root <root@LAPTOP-5K5T48B5>
2023-04-11 08:16:15 +0000

    cpystr

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *copystr;
+
+	int i, n;
+
+	int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[n] != '\0')
+	{
+		len++;
+		n++;
+	}
+
+	copystr = malloc(sizeof(char) * (len + 1));
+
+	for (i = 0; str[i]; i++)
+	{
+		copystr[i] = str[i];
+	}
+	return (copystr);
+}
+
+
+
