c5a56ec8d4d28e7635fdea34d97f0c091be62276
root <root@LAPTOP-5K5T48B5>
2023-04-17 15:43:05 +0000

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog - dog infos
+  *@name: name of dog
+  *@age: age of dog
+  *@owner: owner of the dog
+  *
+  *description: long descpription
+  **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
+
+
