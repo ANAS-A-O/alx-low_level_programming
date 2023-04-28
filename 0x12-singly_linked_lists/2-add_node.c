f2822217c92f9d442cdd86cca61d922fbcde2237
ANAS-A-O <aitouali66@gmail.com>
2023-04-28 02:40:38 +0000

    0-MAIN

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,19 +1,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
 /**
-* add_node - adds a new node at the beginning
-* @head: double pointer to the list
-* @str: new string
-* Return: the address of the new element, or NULL if it fails
+* add_node - la foncio ajoute un node au debut de la list
+* @head: c'est un double pointeur de la list
+* @str: la phrase
+* Return: l' adresse ou NULL
 */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *beg;
-	unsigned int lenght = 0;
+	int lenght = 0;
 
-	while (str[lenght])
+	while (str[lenght] != '\0')
 	lenght++;
 
 	beg = malloc(sizeof(list_t));
