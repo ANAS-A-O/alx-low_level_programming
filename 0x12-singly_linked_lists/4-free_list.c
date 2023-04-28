fb777ea055f3f946e9b8b8650b24b2e6cc86f630
ANAS-A-O <aitouali66@gmail.com>
2023-04-28 02:49:12 +0000

    0-MAIN

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,15 +1,14 @@
 #include <stdlib.h>
 #include "lists.h"
-
 /**
-* free_list - frees a linked list
-* @head: list_t list to be freed
+* free_list - cette fonction free la list aves ses elements
+* @head: c' est un pointeur de la list
 */
 void free_list(list_t *head)
 {
 	list_t *rm;
 
-	while (head)
+	while (head != NULL)
 	{
 		rm = head->next;
 		free(head->str);
