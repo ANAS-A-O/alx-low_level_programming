4c2e3d6a960556f78de90a99671fcdfac04fa1a0
ANAS-A-O <aitouali66@gmail.com>
2023-04-30 23:15:47 +0100

    add at index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**
+*insert_nodeint_at_index - insert a node in a certain position
+*@head: double pointer to the list
+*@idx: the position
+*@n: the data of the node
+*Return: the node or NULL
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *sizeptr = *head;
+
+	unsigned int num;
+	listint_t *indexptr;
+
+	indexptr = malloc(sizeof(listint_t));
+
+	if (head == NULL || indexptr == NULL)
+	return (NULL);
+
+	indexptr->n = n;
+
+	indexptr->next = NULL;
+
+
+	if (idx == 0)
+	{
+		indexptr = indexptr->next;
+		*head = indexptr;
+		return (indexptr);
+	}
+	for (num = 0; sizeptr && num < idx; num++)
+	{
+		if (num == idx - 1)
+		{
+			indexptr->next = sizeptr->next;
+			sizeptr->next = indexptr;
+			return (indexptr);
+		}
+		else
+			sizeptr = sizeptr->next;
+	}
+	return (NULL);
+}
